ea6a9bb5cbaad5c5e1a84a169a23ed9e0a7be661
EC2 Default User <ec2-user@ip-172-31-28-66.ca-central-1.compute.internal>
2024-01-08 21:35:35 +0000

    .

--- terraform/vpc-module/main.tf
+++ terraform/vpc-module/main.tf
@@ -9,6 +9,8 @@ module "my_vpc" {
   azs           = ["ca-central-1a", "ca-central-1b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
 }
 
 module "my_eks_cluster" {
