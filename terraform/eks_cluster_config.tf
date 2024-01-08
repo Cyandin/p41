a4031516e5c1aaf4bed3c471cc8ba8616b8bc47f
EC2 Default User <ec2-user@ip-172-31-28-66.ca-central-1.compute.internal>
2024-01-08 20:52:51 +0000

    more

--- terraform/eks_cluster_config.tf
+++ terraform/eks_cluster_config.tf
@@ -1,15 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~>4.0"
+  }
+ }
+}
+
 provider "aws" {
   region = "ca-central-1"
 }
 
 module "my_vpc" {
   source        = "terraform-aws-modules/vpc/aws"
-  version       = "2.78.0"
   name          = "p41-vpc"
-  cidr          = "10.0.0.0/16"
+  cidr          = "192.0.0.0/16"
   azs           = ["ca-central-1a", "ca-central-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
+  private_subnets = ["192.0.1.0/24", "192.0.2.0/24"]
+  public_subnets  = ["192.0.3.0/24", "192.0.4.0/24"]
 }
 
 module "my_eks_cluster" {
