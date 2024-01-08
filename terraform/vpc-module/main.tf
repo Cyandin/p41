3386b2081c0582e41813d1eaa152807af6237842
EC2 Default User <ec2-user@ip-172-31-28-66.ca-central-1.compute.internal>
2024-01-08 22:25:51 +0000

    .

--- terraform/vpc-module/main.tf
+++ terraform/vpc-module/main.tf
@@ -7,8 +7,8 @@ module "my_vpc" {
   name          = "p41-vpc"
   cidr          = var.cidr
   azs           = ["ca-central-1a", "ca-central-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
+  private_subnets = ["192.0.1.0/24", "192.0.2.0/24"]
+  public_subnets  = ["192.0.3.0/24", "192.0.4.0/24"]
   enable_nat_gateway     = true
   single_nat_gateway     = true
 }
